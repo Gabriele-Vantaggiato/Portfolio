5866b355bdd1343c22468b2ba313e0c6a3c077f1
Gabriele-Vantaggiato <work.gabriele.vantaggiato@gmail.com>
2025-06-01 03:46:03 +0200

    about me page work in progress

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
 import NavbarComponent from '../core/navbar/navbar.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   imports: [
     HlmButtonDirective,
-    NavbarComponent
+    NavbarComponent,
+    RouterLink
   ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
