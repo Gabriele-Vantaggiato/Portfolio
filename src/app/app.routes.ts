75e283aea1ed35375241d9ba0e03424f72746f30
Gabriele-Vantaggiato <work.gabriele.vantaggiato@gmail.com>
2025-06-18 21:26:42 +0200

    test image + fixes navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/home',  pathMatch: 'full'},
   {path: 'home', data: {title: 'HOME.TITLE'}, loadComponent: () => import('./components/hero/hero.component')},
   {path: 'about', data: {title: 'ABOUT_ME.TITLE'}, loadComponent: () => import('./features/about/about.component')},
   {path: 'skills', data: {title: 'SKILLS.TITLE'}, loadComponent: () => import('./features/skill/skill.component')},
   {path: 'certifications', data: {title: 'CERTIFICATIONS.TITLE'}, loadComponent: () => import('./features/certifications/certifications.component')},
   {path: 'experience', data: {title: 'EXPERIENCE.TITLE'}, loadComponent: () => import('./features/experience/experience.component')},
   {path: 'contact', data: {title: 'CONTACT_ME.TITLE'}, loadComponent: () => import('./features/contact/contact.component')},
+  {path: '', redirectTo: '/home',  pathMatch: 'full'},
 ];
