df57ae80252169e5c6fea64e0172f605ff42b7d9
Gabriele-Vantaggiato <work.gabriele.vantaggiato@gmail.com>
2025-06-18 05:18:08 +0200

    minor ui fixes

--- src/app/components/shared/personal-description/personal-description.component.ts
+++ src/app/components/shared/personal-description/personal-description.component.ts
@@ -1,10 +1,14 @@
 import {booleanAttribute, Component, Input} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-personal-description',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    RouterLink,
+    NgClass
   ],
   templateUrl: './personal-description.component.html',
   styleUrl: './personal-description.component.css'
